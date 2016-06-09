e23370728d221d03d592652aef1b2adfbe0977f4
Szabolcs Borbely <borbely.szabolcs@ericsson.com>
2016-06-09 17:31:49 +0200

    README.md+build.gradle added, main() written
    Integration tests are still TO-DO
    The FizzBuzz program can now be used as described in the README file.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # practice
+
+If the code does not meet your expectations, please visit [this page](http://www.dailykitten.com)
+
+# Fizzbuzz
+![FizzBuzzBanner](https://s-media-cache-ak0.pinimg.com/236x/49/11/e1/4911e1adefbe7c9ca7f07c29c29a111d.jpg)
+## How to build
+	cd practice
+	gradle fizzbuzz
+## How to run
+	java -jar build/libs/fizzbuzz.jar IntegerArg1 IntegerArg2
+	
--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -5,26 +5,6 @@ import java.util.ArrayList;
 
 public class FizzBuzz{
 
-	public List<String> generateSequence(Integer range_floor, Integer range_top){
-		List<String> result_list = new ArrayList<String>();
-		boolean divisible_by_3, divisible_by_5;
-
-		for (Integer i=range_floor;i<=range_top;i++){
-			divisible_by_3 = i%3 == 0;
-			divisible_by_5 = i%5 == 0;
-
-			if ( divisible_by_3 & divisible_by_5 )
-				result_list.add("fizzbuzz");
-			else if ( divisible_by_3 )
-				result_list.add("fizz");
-			else if ( divisible_by_5 )
-				result_list.add("buzz");
-			else
-				result_list.add(i.toString());
-		}
-		return result_list;
-	}
-
 	public List<String> fizzBuzzSequence(String range_floor, String range_top){
 		if (rangeIsValid(range_floor, range_top)){
 			Integer floor = Integer.parseInt(range_floor);
@@ -48,9 +28,37 @@ public class FizzBuzz{
 		}
 		return true;
 	}
+	
+	public List<String> generateSequence(Integer range_floor, Integer range_top){
+		List<String> result_list = new ArrayList<String>();
+		boolean divisible_by_3, divisible_by_5;
+
+		for (Integer i=range_floor;i<=range_top;i++){
+			divisible_by_3 = i%3 == 0;
+			divisible_by_5 = i%5 == 0;
+
+			if ( divisible_by_3 & divisible_by_5 )
+				result_list.add("fizzbuzz");
+			else if ( divisible_by_3 )
+				result_list.add("fizz");
+			else if ( divisible_by_5 )
+				result_list.add("buzz");
+			else
+				result_list.add(i.toString());
+		}
+		return result_list;
+	}
 
 	public static void main(String[] args){
+		if (args.length!=2){
+			System.out.println("ERROR: Please use exactly 2 Integer arguments!");
+			return;
+		}
 		
+		new FizzBuzz().fizzBuzzSequence(args[0], args[1])
+			.stream()
+			.forEach(i -> System.out.print(i+", "));
+		System.out.println();
 	}
 
 }
